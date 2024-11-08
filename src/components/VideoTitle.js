7a6db80fedfb51d60d891ea5bed138160bdf1d87
jpofficials7 <jpofficials7@gmail.com>
2024-11-08 17:47:43 +0530

    updates

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -4,8 +4,8 @@ const VideoTitle = ({ title, overView }) => {
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/4">{overView}</p>
       <div>
-        <button className="bg-gray-500 text-white p-4 px-16 text-lg">▶️Play</button>
-        <button>More Info</button>
+        <button className="bg-gray-500 text-white p-4 px-12 text-lg bg-opacity-50 rounded-lg">▶️Play</button>
+        <button className="mx-2 bg-gray-500 text-white p-4 px-12 text-lg bg-opacity-50 rounded-lg">More Info</button>
       </div>
     </div>
   );
